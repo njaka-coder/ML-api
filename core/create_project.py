dec0efebf4a1926bff21c16e7f086c801b32937c
njaka-coder <hasimanjakan@gmail.com>
2025-11-12 14:43:41 +0300

    Keyerror:timeseries_settings fixed for model creation.

--- core/create_project.py
+++ core/create_project.py
@@ -12,17 +12,18 @@ def customeProjectName(projectName: str):
 def customeModelName(projectName: str, modelName: str, window: int, horizon: int):
     try:
         project = server.get_project(projectName)
-        model = project.create_model(
-            modelName,
-            predict='montant_journalier',
-            query='SELECT * FROM sigfp_source.ml_tresorerie',
-            options={
-                'engine': 'neuralforecast',
-                'order_by': 'journalier',
-                'group_by': 'rubrique',
+        model = project.models.create(
+        modelName,
+        predict='montant_journalier',
+        engine= 'neuralforecast',
+        query='SELECT * FROM sigfp_source.ml_tresorerie',
+                timeseries_options={
+                'order': 'journalier',
+                'group': 'rubrique',
                 'window': window,
                 'horizon': horizon
-            })
+                }
+        )
         return model
     except Exception as e:
         raise RuntimeError(f"Erreur création modèle: {e,}")
